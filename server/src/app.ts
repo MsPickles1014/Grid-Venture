545b4f60a69988a1e743110f9249fdd7cf92d541
Ms Pickles1014 <177317399+MsPickles1014@users.noreply.github.comgit config --global user.emailgit config --global user.email noeladnelson@gmail.com>
2025-04-08 20:51:04 -0400

    added cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -13,7 +13,29 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // Allow requests with no origin (like mobile apps, curl requests)
+      if (!origin) return callback(null, true);
 
+      if (origin.includes("localhost")) return callback(null, true);
+
+      if (origin.includes("vercel.app")) return callback(null, true);
+
+      if (origin.includes("onrender.com")) return callback(null, true);
+
+
+      // Otherwise, deny the request
+      
+      callback(new Error("Not allowed by CORS"));
+    },
+
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 // Routes
 
 app.use('/api/auth', authRoutes);
